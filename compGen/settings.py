3c2c34650b5ca672f2b45a0d974293fd3306bf27
Ben Hitz <hitz@genome.stanford.edu>
2012-01-05 16:26:07 -0800

    porting to laptop

--- compGen/settings.py
+++ compGen/settings.py
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/craig/SGD-Strains/mytemplates',
+    '/Users/hitz/Documents/workspace/SGD-Strains/mytemplates',
 )
 
 INSTALLED_APPS = (
